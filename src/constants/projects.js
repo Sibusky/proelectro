911daaa087a95cba2ab6ee69ff91fed299429fd4
Aleksei Smirnov <sibusky@yandex.ru>
2023-04-06 21:40:49 +0400

    feat: add photos of following projects: Retro, Yurovo, Other

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -335,8 +335,7 @@ export const projects = [
   {
     id: 6,
     title: 'Сборка трёхфазного щита для загородного дома в д. Тимонино',
-    description:
-      'Трёхфазный щит на базе автоматики Resi9.',
+    description: 'Трёхфазный щит на базе автоматики Resi9.',
     images: [
       {
         id: 1,
@@ -379,6 +378,202 @@ export const projects = [
         caption: 'Описание 7',
         link: require('../images/projects/timonino/07.jpg'),
         preview: require('../images/projects/timonino/preview/07.jpg'),
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: 'Электропроводка в стиле "ретро"',
+    description: 'Ретро с разных объектов',
+    images: [
+      {
+        id: 1,
+        caption: 'Описание 1',
+        link: require('../images/projects/retro/01.jpg'),
+        preview: require('../images/projects/retro/preview/01.jpg'),
+      },
+      {
+        id: 2,
+        caption: 'Описание 2',
+        link: require('../images/projects/retro/02.jpg'),
+        preview: require('../images/projects/retro/preview/02.jpg'),
+      },
+      {
+        id: 3,
+        caption: 'Описание 3',
+        link: require('../images/projects/retro/03.jpg'),
+        preview: require('../images/projects/retro/preview/03.jpg'),
+      },
+      {
+        id: 4,
+        caption: 'Описание 4',
+        link: require('../images/projects/retro/04.jpg'),
+        preview: require('../images/projects/retro/preview/04.jpg'),
+      },
+      {
+        id: 5,
+        caption: 'Описание 5',
+        link: require('../images/projects/retro/05.jpg'),
+        preview: require('../images/projects/retro/preview/05.jpg'),
+      },
+      {
+        id: 6,
+        caption: 'Описание 6',
+        link: require('../images/projects/retro/06.jpg'),
+        preview: require('../images/projects/retro/preview/06.jpg'),
+      },
+      {
+        id: 7,
+        caption: 'Описание 7',
+        link: require('../images/projects/retro/07.jpg'),
+        preview: require('../images/projects/retro/preview/07.jpg'),
+      },
+      {
+        id: 8,
+        caption: 'Описание 8',
+        link: require('../images/projects/retro/08.jpg'),
+        preview: require('../images/projects/retro/preview/08.jpg'),
+      },
+      {
+        id: 9,
+        caption: 'Описание 9',
+        link: require('../images/projects/retro/09.jpg'),
+        preview: require('../images/projects/retro/preview/09.jpg'),
+      },
+      {
+        id: 10,
+        caption: 'Описание 10',
+        link: require('../images/projects/retro/10.jpg'),
+        preview: require('../images/projects/retro/preview/10.jpg'),
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: 'Сборка трёхфазного щита с программируемым реле',
+    description:
+      'Сборка трёхфазного щита с программируемым реле ProRelay от EKF',
+    images: [
+      {
+        id: 1,
+        caption: 'Описание 1',
+        link: require('../images/projects/yurovo/01.jpg'),
+        preview: require('../images/projects/yurovo/preview/01.jpg'),
+      },
+      {
+        id: 2,
+        caption: 'Описание 2',
+        link: require('../images/projects/yurovo/02.jpg'),
+        preview: require('../images/projects/yurovo/preview/02.jpg'),
+      },
+      {
+        id: 3,
+        caption: 'Описание 3',
+        link: require('../images/projects/yurovo/03.jpg'),
+        preview: require('../images/projects/yurovo/preview/03.jpg'),
+      },
+      {
+        id: 4,
+        caption: 'Описание 4',
+        link: require('../images/projects/yurovo/04.jpg'),
+        preview: require('../images/projects/yurovo/preview/04.jpg'),
+      },
+      {
+        id: 5,
+        caption: 'Описание 5',
+        link: require('../images/projects/yurovo/05.jpg'),
+        preview: require('../images/projects/yurovo/preview/05.jpg'),
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: 'Разные фото рабочего процесса',
+    description: 'Фотографии рабочего процесса и не только',
+    images: [
+      {
+        id: 1,
+        caption: 'Описание 1',
+        link: require('../images/projects/wip/01.jpg'),
+        preview: require('../images/projects/wip/preview/01.jpg'),
+      },
+      {
+        id: 2,
+        caption: 'Описание 2',
+        link: require('../images/projects/wip/02.jpg'),
+        preview: require('../images/projects/wip/preview/02.jpg'),
+      },
+      {
+        id: 3,
+        caption: 'Описание 3',
+        link: require('../images/projects/wip/03.jpg'),
+        preview: require('../images/projects/wip/preview/03.jpg'),
+      },
+      {
+        id: 4,
+        caption: 'Описание 4',
+        link: require('../images/projects/wip/04.jpg'),
+        preview: require('../images/projects/wip/preview/04.jpg'),
+      },
+      {
+        id: 5,
+        caption: 'Описание 5',
+        link: require('../images/projects/wip/05.jpg'),
+        preview: require('../images/projects/wip/preview/05.jpg'),
+      },
+      {
+        id: 6,
+        caption: 'Описание 6',
+        link: require('../images/projects/wip/06.jpg'),
+        preview: require('../images/projects/wip/preview/06.jpg'),
+      },
+      {
+        id: 7,
+        caption: 'Описание 7',
+        link: require('../images/projects/wip/07.jpg'),
+        preview: require('../images/projects/wip/preview/07.jpg'),
+      },
+      {
+        id: 8,
+        caption: 'Описание 8',
+        link: require('../images/projects/wip/08.jpg'),
+        preview: require('../images/projects/wip/preview/08.jpg'),
+      },
+      {
+        id: 9,
+        caption: 'Описание 9',
+        link: require('../images/projects/wip/09.jpg'),
+        preview: require('../images/projects/wip/preview/09.jpg'),
+      },
+      {
+        id: 10,
+        caption: 'Описание 10',
+        link: require('../images/projects/wip/10.jpg'),
+        preview: require('../images/projects/wip/preview/10.jpg'),
+      },
+      {
+        id: 11,
+        caption: 'Описание 11',
+        link: require('../images/projects/wip/11.jpg'),
+        preview: require('../images/projects/wip/preview/11.jpg'),
+      },
+      {
+        id: 12,
+        caption: 'Описание 12',
+        link: require('../images/projects/wip/12.jpg'),
+        preview: require('../images/projects/wip/preview/12.jpg'),
+      },
+      {
+        id: 13,
+        caption: 'Описание 13',
+        link: require('../images/projects/wip/13.jpg'),
+        preview: require('../images/projects/wip/preview/13.jpg'),
+      },
+      {
+        id: 14,
+        caption: 'Описание 14',
+        link: require('../images/projects/wip/14.jpg'),
+        preview: require('../images/projects/wip/preview/14.jpg'),
       },
     ],
   },
