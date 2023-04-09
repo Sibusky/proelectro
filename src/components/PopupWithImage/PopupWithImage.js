a6294ba328ab501594ec7585837c8f0643dd90b5
Aleksei Smirnov <sibusky@yandex.ru>
2023-04-09 21:05:00 +0400

    feat: add logo in project images popup

--- src/components/PopupWithImage/PopupWithImage.js
+++ src/components/PopupWithImage/PopupWithImage.js
@@ -57,6 +57,7 @@ export default function PopupWithImage({
                         {text}
                       </p>
                     ))}
+                    <div className='popup__carousel-image-caption-logo'></div>
                   </div>
                 </SwiperSlide>
               ))
