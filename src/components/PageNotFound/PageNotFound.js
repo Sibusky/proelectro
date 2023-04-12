5ee40a18a094ef7905f97bd12a21f9e04aad98b1
Aleksei Smirnov <sibusky@yandex.ru>
2023-04-12 21:38:13 +0400

    refactor: change homepage

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function PageNotFound() {
-  // let navigate = useNavigate();
-  // let location = useLocation();
+  let navigate = useNavigate();
+  let location = useLocation();
 
-  // // Это исключительно для GH-Pages
-  // useEffect(() => {
-  //   if (
-  //     location.pathname === '/proelectro/' ||
-  //     location.pathname === '/proelectro'
-  //   ) {
-  //     navigate('/');
-  //   }
-  // }, [navigate, location.pathname]);
+  // Это исключительно для GH-Pages
+  useEffect(() => {
+    if (
+      location.pathname === '/proelectro/' ||
+      location.pathname === '/proelectro'
+    ) {
+      navigate('/');
+    }
+  }, [navigate, location.pathname]);
 
   return (
     <div>
