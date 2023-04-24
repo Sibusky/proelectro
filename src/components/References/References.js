870c01561e870d5065cd84d100ff1c7f0ec74fc9
Aleksei Smirnov <sibusky@yandex.ru>
2023-04-24 22:43:39 +0400

    feat: add references from VK, next step is to set it up

--- src/components/References/References.js
+++ src/components/References/References.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import './References.css';
 
 export default function References() {
+  //Настройка линтера, чтобы не ругался на отсутсвие feedback_vk
+  /* global feedback_vk */
+
+  // Загрузка отзывов
+
+  useEffect(() => {
+    document.addEventListener(
+      'DOMContentLoaded',
+      feedback_vk.init({ id: 'feedback_vk', gid: 114151564, count: 100 })
+    );
+    return document.removeEventListener(
+      'DOMContentLoaded',
+      feedback_vk.init({ id: 'feedback_vk', gid: 114151564, count: 100 })
+    );
+  }, []);
+
   return (
-    <div>
-        <h3>References</h3>
-    </div>
-  )
+    <main className='references'>
+      <section className='feedback section'>
+        <div className='feedback__container section__container'>
+          <h2 className='feedback__title section__title'>
+            Отзывы о нашей работе
+          </h2>
+          <div className='feedback_vk' id='feedback_vk'></div>
+        </div>
+      </section>
+    </main>
+  );
 }
